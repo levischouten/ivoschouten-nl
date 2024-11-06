14efc09f2662ddcec90a29b8b65b7b4d5cb1a269
Levi Schouten <levi.schouten.werk@gmail.com>
2024-11-06 14:05:41 +0100

    refactor: Update storage kind based on environment

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,7 @@ import { config, fields, collection } from "@keystatic/core";
 
 export default config({
   storage: {
-    kind: "local",
+    kind: process.env.NODE_ENV === "development" ? "local" : "cloud",
   },
   ui: {
     brand: {
