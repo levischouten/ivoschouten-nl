530a5cf5e32ee59e3604e67111d300c79ff9c7e5
Levi Schouten <levi.schouten.werk@gmail.com>
2024-11-21 20:17:52 +0100

    Update footer component and modify about section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,9 @@
 export function Footer() {
   return (
-    <footer className="w-full text-xs max-w-screen-lg px-8">
+    <footer className="w-full text-[10px] lg:text-xs max-w-screen-lg px-8">
       {/* <hr className="w-full border-t border-gray-400" /> */}
-      <div className="w-full flex justify-between pt-2 pb-8 mx-auto text-muted-foreground">
-        <p>Lorum Ipsum</p>
-        <p className="text-center">© {new Date().getFullYear()} Your Company</p>
+      <div className="w-full pt-2 pb-8 mx-auto text-muted-foreground">
+        <p className="text-center">© {new Date().getFullYear()} Ivo Schouten</p>
       </div>
     </footer>
   );
