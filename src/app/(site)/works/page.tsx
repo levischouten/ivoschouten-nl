426e1c8feec3898157d45cda18eaf54fdbc92208
Levi Schouten <levi.schouten.werk@gmail.com>
2024-11-26 22:26:28 +0100

    update image grid

--- src/app/(site)/works/page.tsx
+++ src/app/(site)/works/page.tsx
@@ -11,19 +11,19 @@ export default async function Page() {
 
   return (
     <div className="mt-6">
-      <ul className="grid grid-cols-2 sm:grid-cols-3 gap-8">
+      <ul className="grid grid-cols-2 sm:grid-cols-3 gap-4">
         {posts.map((post) => (
           <li key={post.slug}>
-            <Link href={`/works/${post.slug}`} className="space-y-4">
+            <Link href={`/works/${post.slug}`} className="space-y-1 group">
               <Image
                 src={post.entry.image}
                 alt={post.entry.title}
                 width={320}
                 height={180}
-                className="h-auto w-full object-cover mx-auto aspect-square"
+                className="h-auto w-full object-cover mx-auto aspect-square grayscale transition-all duration-300 group-hover:grayscale-0"
               />
               <div>
-                <h2 className="text-sm">{post.entry.title}</h2>
+                <h2 className="text-xs">{post.entry.title}</h2>
               </div>
             </Link>
           </li>
